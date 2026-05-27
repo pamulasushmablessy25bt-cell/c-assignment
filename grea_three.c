3a27d4552160809aa52c7e6dd7cea6f29480a739
pamulasushmablessy25bt-cell <pamulasushmablessy.25.bt@anits.edu.in>
2026-05-27 15:31:23 +0530

    Create grea_three.c

---
+++ grea_three.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    // Input three numbers
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // Find greatest using nested if-else
+    if (a > b) {
+        if (a > c) {
+            printf("%d is the greatest number.", a);
+        } else {
+            printf("%d is the greatest number.", c);
+        }
+    } else {
+        if (b > c) {
+            printf("%d is the greatest number.", b);
+        } else {
+            printf("%d is the greatest number.", c);
+        }
+    }
+
+    return 0;
+}
