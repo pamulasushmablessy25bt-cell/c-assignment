db7738adbec7fa9eeb3edfadf700a86e92893bb7
pamulasushmablessy25bt-cell <pamulasushmablessy.25.bt@anits.edu.in>
2026-05-27 15:36:51 +0530

    Create recur_fun.c

---
+++ recur_fun.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Recursive function to find factorial
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &num);
+
+    printf("Factorial of %d = %d", num, factorial(num));
+
+    return 0;
+}
